9dfe6e75fed71dea5123ef46dd62cbfd6a80ce35
Daniel Guryev <Daniel.Guryev@gmail.com>
2025-07-02 09:18:30 +0100

    Implement part 2 using arrays

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,16 +3,30 @@
 for (let i=1; i<=100; i++){
     
 
-    let out = ""
-    if (i%3==0)
-        out+="Fizz"
-    if (i%5==0)
-        out+="Buzz"
+    let out = []
+     if (i%11==0)
+        out.push("Bong")
+    else {
+        if (i%3==0)
+            out.push("Fizz")
+        if (i%5==0)
+            out.push("Buzz")
+        if (i%7==0)
+            out.push("Bang")
+    }
+    if (i%13==0){
+        const fb = out.find((i)=>{return i[0]==="B"})
+        if (fb!=undefined)
+            out.splice(fb,0,"Fezz")
+        else
+            out.push("Fezz")
+    }
+    if (i%17==0){
+        out.reverse()
+    }
+    if (out.length==0)
+        out.push(i.toString())
 
-
-    if (out==="")
-        out=i.toString()
-
-    console.log(out)
+    console.log(out.reduce((prev,cur)=>{return prev+cur},""))
 
 }
