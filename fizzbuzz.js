30ac1eb3dff5bfbe5e7094e4a17988f912b92de6
Daniel Guryev <Daniel.Guryev@gmail.com>
2025-07-02 09:57:36 +0100

    Implement stretch goal 1

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,35 +1,48 @@
 
 
+// since we're using nodejs, we'll read console input using node:readline as per
+// https://nodejs.org/en/learn/command-line/accept-input-from-the-command-line-in-nodejs
 
+const readline = require('node:readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-for (let i=1; i<=256; i++){
-    
+// There doesn't seem to be a blocking reader (like scanf) available, 
+// so we'll wrap our program into a callback to rl.question()
+rl.question("Enter a number: ", (limit)=>{
 
-    let out = [];
-    if (i%11==0)
-        out.push("Bong");
-    else {
-        if (i%3==0)
-            out.push("Fizz");
-        if (i%5==0)
-            out.push("Buzz");
-        if (i%7==0)
-            out.push("Bang");
+    for (let i=1; i<=limit; i++){
+        
+
+        let out = [];
+        if (i%11==0)
+            out.push("Bong");
+        else {
+            if (i%3==0)
+                out.push("Fizz");
+            if (i%5==0)
+                out.push("Buzz");
+            if (i%7==0)
+                out.push("Bang");
+        }
+        if (i%13==0){
+            const fb = out.findIndex((i)=>{return i[0]==="B"});
+            if (fb>=0)
+                out.splice(fb,0,"Fezz");
+            else
+                out.push("Fezz");
+        }
+        if (i%17==0){
+            out.reverse();
+        }
+        if (out.length==0)
+            out.push(i.toString());
+
+        console.log(out.reduce((prev,cur)=>{return prev+cur},""));
+
     }
-    if (i%13==0){
-        const fb = out.findIndex((i)=>{return i[0]==="B"});
-        if (fb>=0)
-            out.splice(fb,0,"Fezz");
-        else
-            out.push("Fezz");
-    }
-    if (i%17==0){
-        out.reverse();
-    }
-    if (out.length==0)
-        out.push(i.toString());
 
-    console.log(out.reduce((prev,cur)=>{return prev+cur},""));
-
-}
-
+    rl.close()
+})
