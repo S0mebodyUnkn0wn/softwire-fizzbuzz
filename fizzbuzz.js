fb29c1267e399a466d35ffee513bceb2e6f2b214
Daniel Guryev <Daniel.Guryev@gmail.com>
2025-07-02 09:56:14 +0100

    Fix part 2 Previous commit was an out-of-date version of the program

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,32 +1,35 @@
 
 
-for (let i=1; i<=100; i++){
+
+
+for (let i=1; i<=256; i++){
     
 
-    let out = []
-     if (i%11==0)
-        out.push("Bong")
+    let out = [];
+    if (i%11==0)
+        out.push("Bong");
     else {
         if (i%3==0)
-            out.push("Fizz")
+            out.push("Fizz");
         if (i%5==0)
-            out.push("Buzz")
+            out.push("Buzz");
         if (i%7==0)
-            out.push("Bang")
+            out.push("Bang");
     }
     if (i%13==0){
-        const fb = out.find((i)=>{return i[0]==="B"})
-        if (fb!=undefined)
-            out.splice(fb,0,"Fezz")
+        const fb = out.findIndex((i)=>{return i[0]==="B"});
+        if (fb>=0)
+            out.splice(fb,0,"Fezz");
         else
-            out.push("Fezz")
+            out.push("Fezz");
     }
     if (i%17==0){
-        out.reverse()
+        out.reverse();
     }
     if (out.length==0)
-        out.push(i.toString())
+        out.push(i.toString());
 
-    console.log(out.reduce((prev,cur)=>{return prev+cur},""))
+    console.log(out.reduce((prev,cur)=>{return prev+cur},""));
 
 }
+
